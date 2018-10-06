1516e63b0e1f0c8d23082f0b281fb915360be0ca
Maksim Zaitsev <max.zaicev@gmail.com>
2018-10-06 12:53:34 +0300

    Update project

--- src/main/java/sparkmllib.scala
+++ src/main/java/sparkmllib.scala
@@ -1,5 +1,32 @@
-object sparkmllib {
-    void main(){
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
 
-    }
+object SparkMLlib {
+
+  def main(args: Array[String]): Unit = {
+
+    val inputFile = "C:\\Users\\maxza\\Desktop\\sparkmllib\\src\\main\\resources\\retails.csv"
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    //Initialize SparkSession
+    val sparkSession = SparkSession
+      .builder()
+      .appName("spark-read-csv")
+      .master("local[*]")
+      .getOrCreate();
+
+    //Read file to DF
+    val crimes = sparkSession.read
+      .option("header", "true")
+      .option("delimiter", ",")
+      .option("nullValue", "")
+      .option("treatEmptyValuesAsNulls", "true")
+      .option("inferSchema", "true")
+      .csv(inputFile)
+
+    crimes.show(100)
+    crimes.printSchema();
+
+  }
 }
